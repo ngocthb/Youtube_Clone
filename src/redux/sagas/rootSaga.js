bd98f10deabbb040cdd0a038b38979d7f7b45464
DatTien2107 <chytai2012@gmail.com>
2025-02-09 09:03:43 +0700

    update , fix screen videos trending

--- src/redux/sagas/rootSaga.js
+++ src/redux/sagas/rootSaga.js
@@ -1,7 +1,8 @@
 import { all } from "redux-saga/effects";
-import VideosSaga from "./VideoSaga";
+import videoSaga from "./VideoSaga";
 import SearchSaga from "./searchSaga";
 
+
 export default function* rootSaga() {
-  yield all([VideosSaga(), SearchSaga()]);
+  yield all([videoSaga(), SearchSaga()]);
 }
